8e314789f107420ed700ccad10c032f8e4fa8bd5
grantba <grant.blaire@gmail.com>
2021-08-13 05:31:33 -0500

    Deleted component file

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,15 +5,15 @@ const Movies = props => {
         return(
             <div><h2 className="errorMessage">{props.movie.errors}</h2></div>
         )
-    } else if (props.movie.data !== undefined) {
+    } else if (props.movie !== undefined) {
         return(
-            <div key={props.movie.data.id} id={props.movie.data.id} className="movieComponent">
-                <img src={props.movie.data.attributes.Poster} alt={props.movie.data.attributes.Title}></img><br></br>
-                <h1>{props.movie.data.attributes.Title} - {props.movie.data.attributes.Year}</h1>
-                <h3>Plot: {props.movie.data.attributes.Plot}</h3>
-                <h3>Genre: {props.movie.data.attributes.Genre}</h3>
-                <h3>Runtime: {props.movie.data.attributes.Runtime}</h3>
-                <h3>Rated: {props.movie.data.attributes.Rated}</h3>
+            <div key={props.movie.id} id={props.movie.id} className="movieComponent">
+                <img src={props.movie.attributes.Poster} alt={props.movie.attributes.Title}></img><br></br>
+                <h1>{props.movie.attributes.Title} - {props.movie.attributes.Year}</h1>
+                <h3>Plot: {props.movie.attributes.Plot}</h3>
+                <h3>Genre: {props.movie.attributes.Genre}</h3>
+                <h3>Runtime: {props.movie.attributes.Runtime}</h3>
+                <h3>Rated: {props.movie.attributes.Rated}</h3>
             </div>
             )    
     } else {
--- src/containers/MoviesContainer.js
+++ src/containers/MoviesContainer.js
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-import React, {Component} from 'react'
-import MoviesShow from '../components/MoviesShow'
-
-class MoviesContainer extends Component {
-    render() {
-        return (
-            <div>
-                <MoviesShow movie={this.props.movies}/>
-            </div>
-        )
-    }
-}
-
-export default MoviesContainer
-=======
 import React, {Component} from "react";
-import Movies from "../component/Movies";
+import Movies from "../components/Movies";
 
 class MoviesContainer extends Component {
     state = {
@@ -36,13 +20,10 @@ class MoviesContainer extends Component {
         return (
             <div>
                 <h1>Movies Container</h1>
-                <ul>
-                    {movies ? movies.map(movie => <li key={movie.id}><Movies movie={movie} /></li>) : "Loading"}
-                </ul>
+                {movies ? movies.map(movie => <Movies key={movie.id} movie={movie}/>) : "Loading"}
             </div>   
         );
     }
 }
 
 export default MoviesContainer;
->>>>>>> b301ab5b150c6f1b86995018519d66a0a8225abe
